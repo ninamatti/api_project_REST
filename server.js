a6b6685658b0a077d28578c6066ccdb8eca163e2
Nina Matti <ninadesireematti@gmail.com>
2020-10-20 10:31:32 +0900

    updated server.js, removed console.logs etc, all endpoints work

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const knex = require('knex')({
 
 //const knex = require("./knexfile");
 
-// check if we can acces user data
+// define games data
 const games = knex
     .select()
     .table('game')
@@ -26,67 +26,89 @@ const express = require("express");
 const app = express();
 
 // middleware: app.use!
-app.use(express.static("./"));
-
-app.get("/hello", (req, res) => {
-    res.send("Hello world!");
-});
+app.use(express.static("./client"));
 
 app.get("/gamesearcher", async (req, res) => {
     knex
     .from('game')
     .select()
     .then((data) => {
-    console.log(data);
     res.send(data);
     })
-    
 });
 
 app.get("/gamesearcher/:gamename", (req, res) => {
-    // const name = req.params.gamename;
-    // const result = [];
-    // for(let game of games) {
-    //     if(game.name === name) {
-    //         result.push(game);
-    //     }
-    // }
-    // res.send(result);
+    knex
+    .from('game')
+    .select()
+    .where({ name: req.params.gamename})
+    .then((data) => {
+        res.send(data);
+    })
 });
 
-
-app.post("/gamesearcher/:gamename", (req, res) => {
-    // const id = req.params.id;
-    // const name = req.params.name;
-    // pokeData.pokemon.push({ id, name }); //the array
-    // res.sendStatus(201);
+app.get("/gamesearcher/genres/:genrename", async (req, res) => {
+    knex
+    .from('game')
+    .select()
+    .then((data) => {
+        return data.filter((game) => {
+            return game.genre.includes(req.params.genrename);
+        })
+    })
+    .then((genregames) => {
+        res.send(genregames);
+    }) 
 });
 
-app.patch("/gamesearcher/:gamename", (req, res) => {
-    
-    
-    // let Index = 0;
-    // const { name } = req.query;
-    // if (Number(req.params.identifier)) {
-    //   const id = Number(req.params.idOrName);
-    //   for (const [index, value] of pokeData.pokemon.entries()) {
-    //     if (Number(value.id) === id) Index = index;
-    //   }
-    // } else {
-    //   const name = req.params.idOrName;
-    //   for (const [index, value] of pokeData.pokemon.entries()) {
-    //     if (value.name === name) Index = index;
-    //   }
-    // }
-    // pokeData.pokemon[Index].name = name;
-    // res.send(pokeData.pokemon[Index]);
+app.get("/gamesearcher/developers/:developername", async (req, res) => {
+    knex
+    .from('game')
+    .select()
+    .then((data) => {
+        return data.filter((game) => {
+            return game.developer_name.includes(req.params.developername);
+        })
+    })
+    .then((devgames) => {
+        res.send(devgames);
+    }) 
+});
+
+app.post("/gamesearcher/:gamename/:developer_name/:genre", async (req, res) => {
+    const developer = req.params.developer_name;
+    const name = req.params.gamename;
+    const genre = req.params.genre;
+    knex('game')
+        .insert({ 
+            name: name,
+            developer_name: developer,
+            genre: genre 
+        })
+        .then(() => {
+            console.log('added new game!');
+            res.sendStatus(201);
+        })
+});
+
+app.patch("/gamesearcher/:gamename/:newgamename", async (req, res) => {
+    knex('game')
+        .where({ name: req.params.gamename })
+        .update({ name: req.params.newgamename })
+        .then(() => {
+            res.sendStatus(200);
+        })
 });  
 
 
-app.delete("/gamesearcher/:gamename", (req, res) => {
+app.delete("/gamesearcher/:gamename", async (req, res) => {
     knex('game')
     .where({ name: req.params.gamename })
-    .del();
+    .del()
+    .then((data) => {
+        res.sendStatus(200)
+    })
+    
 });
 
 
